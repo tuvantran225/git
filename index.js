1372cc70815b1d2b24fb0dec30a9cb85c20cb398
tuvantran225 <tuvantran225@gmail.com>
2019-12-27 12:09:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const pug = new Dog("Punk");
 pug.sayHi();
 
 const jerry = new Mouse("Jerry");
+const micky = new Mouse("Micky");
 const tom = new Cat("Tom");
 tom.eat(jerry);
+tom.eat(micky);
 pug.eat(tom);
 
 console.log(pug);
